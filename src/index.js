2d66efeb89b3a66e5e58c353fa26c0c2d4970723
monzuralam <monzur707@gmail.com>
2022-01-17 14:32:36 +0600

    add attribute & dynamic value

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 wp.blocks.registerBlockType("hello-attention-quiz/are-you-paying-attention", {
     title: "Are You Paying Attention ?",
     icon: "smiley",
+    attributes: {
+        skyColor: {type: "string"},
+        grassColor: {type: "string"},
+    },
     category: "common",
-    edit: function () {
+    edit: function (props) {
+        function updateSkyColor(event){
+            props.setAttributes({skyColor: event.target.value});
+        }
+        
+        function updateGrassColor(event){
+            props.setAttributes({grassColor: event.target.value});
+        }
         return (
             <div>
-                <h2>Attention</h2>
-                <p style={{color: 'green'}}>Paragraph</p>
+                <input type="text" placeholder="Sky Color" value={props.attributes.skyColor} onChange={updateSkyColor} />
+                <input type="text" placeholder="Grass Color" value={props.attributes.grassColor} onChange={updateGrassColor} />
             </div>
         );
     },
-    save: function () {
+    save: function (props) {
         return (
             <div>
-                <h3>Hello Attention Frontend</h3>
+                <p>Today the sky is {props.attributes.skyColor} and the grass is {props.attributes.grassColor}.</p>
             </div>
         );
     }
